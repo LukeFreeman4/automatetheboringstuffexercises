817a4497ea34f3c95a6a1114f20c1fd7a625265d
Luke <luke.freeman4@gmail.com>
2022-12-20 06:42:59 -0600

    automate the boring stuff exercises

---
+++ exercises.py
@@ -0,0 +1,24 @@
+
+#GUESS THE NUMBER
+import random
+random_Number = random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# NAME AND AGE EXERCISE
+# name = input('Hello, What is your name? ')
+# print(f'Hello {name}. It is nice to meet you. the length of your name is {len(name)}')
+# age = int(input('What is your age?'))
+# print(f'You will be {age +1} in a year.')
