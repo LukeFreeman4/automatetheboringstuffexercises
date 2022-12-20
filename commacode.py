0e0023a11c85983113b80795a8b3571e676aa3f2
Luke <luke.freeman4@gmail.com>
2022-12-20 06:01:36 -0600

    Removed all and starting over

--- commacode.py
+++
@@ -1,7 +0,0 @@
-myList = ['cat', 'dog', 'squirrel']
-
-def toString(arg):
-    for i in arg:
-        print("'")
-
-toString(myList)
--- randomTests.py
+++
@@ -1,7 +0,0 @@
-import nacl
-
-
-A={"name": {"firstName": "Luke", "lastName": "Freeman"}, "age": 33, "partnerStatus": "single"}
-
-
-print(A['name']['firstName'] +" " + A['name']['lastName'] + ' is ' + str(A["age"]) + ' years old')
